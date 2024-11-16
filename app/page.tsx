bb420cb99f31f17b932dd140d9fa8ad87a5c0109
shazzad <sumonshazzad980.com>
2024-11-16 17:57:39 +0600

    firs commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import About from "./about/page";
-import Feacher from "./feacher/page";
-import NavBar from "./navbar/page";
-import Service from "./service/page";
-
 const Home = () => {
   return <div className=""></div>;
 };
